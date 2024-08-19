fcbc36955d31ca91766760f28d9b21e222ab58d9
CrystalW11 <crystal.warmack@gmail.com>
2024-08-19 14:23:59 -0600

    Nav.jsx

--- src/views/Blog.jsx
+++ src/views/Blog.jsx
@@ -1,6 +1,6 @@
 /** @format */
 import React from "react";
-import Nav from "../components/Nav";
+import Nav from "../components/Nav.jsx";
 import CityWithFrequencyWaves from "../assets/CityWithFrequencyWaves.png";
 import DigitalCityGridImage from "../assets/DigitalCityGridImage.png";
 
